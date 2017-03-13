a32b12b3265f7135599c489bc7effaebfb8890f8
arekk <arkadiuszkicior@gmail.com>
2017-03-13 12:00:44 +0100

    second commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 var a = prompt ('Wpisz długość boku trójkąta');
-var h = prompt ('Wpisz długość podstawy trójkąta'); 
-triangleArea = a*h/2;
+var h = prompt ('Wpisz wysokość trójkąta'); 
+var triangleArea = a*h/2;
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
